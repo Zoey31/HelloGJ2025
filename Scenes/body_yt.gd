6c699f38c44cc5a1a4015b9ca037e841090cc278
Piotr Masek <piotr3km@gmail.com>
2025-07-05 20:08:43 +0200

    Testy scrolla

--- Scenes/body_yt.gd
+++ Scenes/body_yt.gd
@@ -2,4 +2,11 @@ extends VBoxContainer
 var video = preload("res://Scenes/image.tscn")
 
 func _ready() -> void:
+	print(get_parent().name)
+	add_child(video.instantiate())
+	add_child(video.instantiate())
+	add_child(video.instantiate())
+	add_child(video.instantiate())
+	add_child(video.instantiate())
+	add_child(video.instantiate())
 	add_child(video.instantiate())
